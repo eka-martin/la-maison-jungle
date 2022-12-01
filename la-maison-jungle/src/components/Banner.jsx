69d1e3814634c80695c48df4495c9fe2a52c3674
eka-martin <99816833+eka-martin@users.noreply.github.com>
2022-12-01 19:53:22 +0100

    style

---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import '../styles/Banner.css'
+import logo from '../assets/logo.png'
+
+function Banner() {
+    const title = 'La maison jungle'
+    return <div className='lmj-banner'>
+        <img src={logo} alt='La maison jungle' className='lmj-logo' />
+    <h1 className='lmj-title'>{title}</h1>
+</div>
+}
+
+export default Banner
